c2ae21f53f33a3114ecade3a166891b642ca31bd
HOLFF <leerharter@gmail.com>
2020-01-08 10:44:37 +0100

    addes basic exception handle

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args)throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname","username","password");
       //in dbname datenbanknamen eintragen, in username de username des localhost und in das passwort das passwort.....
