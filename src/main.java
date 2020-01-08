4d745dbb85a4c455e1f48e043864ba7b4d440bbc
matteo juen <k.juen@icloud.com>
2020-01-08 10:41:16 +0100

    SQL Connection

--- src/main.java
+++ src/main.java
@@ -1,6 +1,12 @@
+import java.sql.*;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("dis a project");
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname","username","password");
+      //in dbname datenbanknamen eintragen, in username de username des localhost und in das passwort das passwort.....
+        //Statement stmt = con.createStatement();
+        //ResultSet res = stmt.executeQuery("irgendein SQL Query");
     }
 
 }
